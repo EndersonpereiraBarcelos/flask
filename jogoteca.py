096c40b45cdd27e150521d64342c22520789c2e9
endersonpb <endersonpb0@gmail.com>
2023-11-27 13:02:12 -0300

    meio caminho andado

--- jogoteca.py
+++ jogoteca.py
@@ -1,11 +1,39 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__, template_folder='template')
 
 
+class Jogo:
+    def __init__(self, nome, categoria, console):
+        self.nome = nome
+        self.categoria = categoria
+        self.console = console
+
+
+jogo1 = Jogo('Tetris', 'Puzzle', 'Atari')
+jogo2 = Jogo('God Of War', 'Luta', 'PS4')
+jogo3 = Jogo('Mk', 'Luta', 'Multiplataforma')
+lista = [jogo1, jogo2, jogo3]
+
+
 @app.route('/')
-def ola():
-    lista = ['Tetris', 'God Of War', 'Valorant']
+def home():
+
+    return render_template('lista.html', title='Jogos', jogos=lista)
+
+
+@app.route('/newgame')
+def newgame():
+    return render_template('novo.html', title='Novos Jogos')
+
+
+@app.route('/criar', methods=['POST', ])
+def criar():
+    nome = request.form['nome']
+    categoria = request.form['categoria']
+    console = request.form['console']
+    jogo = Jogo(nome, categoria, console)
+    lista.append(jogo)
     return render_template('lista.html', title='Jogos', jogos=lista)
 
 
